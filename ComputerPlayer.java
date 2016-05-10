071ca8b3659d5303ca8dfa48b83efb02022db6a9
abhivk23 <abhivk23@gmail.com>
2016-05-10 08:55:38 -0500

    Add files via upload
    Player stuff

---
+++ ComputerPlayer.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class ComputerPlayer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ComputerPlayer extends Player
+{
+    private int compShips;
+
+    public ComputerPlayer()
+    {
+        compShips= 5;
+    }
+
+    public int getCompShips()
+    {
+        return getCompShips;
+    }
+}
---
+++ Player.java
@@ -0,0 +1,23 @@
+//Group awesome
+//Battleship
+//May 6
+public class Player
+{
+    private int numShips;
+    
+    public Player()
+    {
+        numShips= 5;
+        
+    }
+    
+    public int getNumShips()
+    {
+        return numShips;
+    }
+    
+    public void takeShot(int x, int y)
+    {
+        
+    }
+}
