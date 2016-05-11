76a67c11b7aaec5d846abc5196d503d1b0460e02
Allison Chen 2018 <achen8@students.d125.org>
2016-05-11 08:45:38 -0500

    working on placeShips in opponent gameboard class

--- GameboardOpponent.java
+++ GameboardOpponent.java
@@ -1,10 +1,10 @@
 //Allison
 public class GameboardOpponent
 {
-    private static ViewSpot [][] board;
+    private static BoardSpot [][] board;
     public GameboardOpponent()
     {
-        board=new ViewSpot[10][10];
+        board=new BoardSpot[10][10];
         for (int row=0; row<10; row++)
         {
             for (int col=0; col<10; col++)
@@ -18,16 +18,32 @@ public class GameboardOpponent
     {
         int ctr=0;
         int[] lens={2,3,3,4,5};
-        int row=(int)(Math.random()*10+1);
-        int col=(int)(Math.random()*10+1);
+        int row;
+        int col;
         while (ctr<5)
         {
             Ship compShip=new Ship(lens[ctr]);
             row=(int)(Math.random()*10+1);
             col=(int)(Math.random()*10+1);
             int vert=(int)(Math.random()*2);
-            //if vert
-
+            if (vert==0)
+            {
+                compShip.setIsVert(true);
+            }
+            else
+            {
+                compShip.setIsVert(false);
+            }
+            if(compShip.getIsVert)
+            {
+                boolean valid=true;
+                for (int i=0; i<len; i++)
+                {
+                    if ((row+i>board.length || col+i>board[0].length) ||
+                    (board[row+i][col+i].containsShip==true))
+                        valid=false;
+                }
+            }
         }
     }
 }
