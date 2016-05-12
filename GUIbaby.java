40a613ff325c9cb779f0b09c729769076fb43f79
bmalik8 <bmalik8@students.d125.org>
2016-05-12 08:34:05 -0500

    Working the GUIbaby

---
+++ GUIbaby.java
@@ -0,0 +1,13 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * Class GUIbaby - write a description of the class here
+ * 
+ * @author (your name) 
+ * @version (a version number)
+ */
+public class GUIbaby extends JApplet
+{
+}
