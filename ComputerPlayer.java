e3c12241c6884a4c210c848168c803c81119d6fa
abhivk23 <abhivk23@gmail.com>
2016-05-25 08:36:35 -0500

    usah dude

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -24,83 +24,93 @@ public class ComputerPlayer extends Player
 
     public void takeShot()
     {
-        if (foundShip = false) {
+        x= (int)(Math.random()*10);
+        y= (int)(Math.random()*10);
+        while((GameboardYou.getBoardSpot(y,x)).getIsHit() == true) {
             x= (int)(Math.random()*10);
             y= (int)(Math.random()*10);
-            while((GameboardYou.getBoardSpot(y,x)).getIsHit() == true) {
-                x= (int)(Math.random()*10);
-                y= (int)(Math.random()*10);
-            }
-            if ((GameboardYou.getBoardSpot(y,x)).getContainsShip() == true)
-            {
-                ((GameboardYou.getBoardSpot(y,x)).getBattleShip()).hit();
-                GameboardYou.getBoardSpot(y,x).setIsHit(true);
-                foundShip = true;
-                counter = 0;
-            } 
+        }
+        ((GameboardYou.getBoardSpot(y,x)).getBattleShip()).hit();
+        (GameboardYou.getBoardSpot(y,x)).setIsHit(true); 
+
+        /*
+        if (foundShip = false) {
+        x= (int)(Math.random()*10);
+        y= (int)(Math.random()*10);
+        while((GameboardYou.getBoardSpot(y,x)).getIsHit() == true) {
+        x= (int)(Math.random()*10);
+        y= (int)(Math.random()*10);
+        }
+        if ((GameboardYou.getBoardSpot(y,x)).getContainsShip() == true)
+        {
+        ((GameboardYou.getBoardSpot(y,x)).getBattleShip()).hit();
+        GameboardYou.getBoardSpot(y,x).setIsHit(true);
+        foundShip = true;
+        counter = 0;
+        } 
         }
         else {            
-            if (counter == 1) {
-                if(x>0) {
-                    if ((GameboardYou.getBoardSpot(y,x-1)).getContainsShip() == true && 
-                    (GameboardYou.getBoardSpot(y,x-1)).getIsHit() == false)
-                    {
-                        ((GameboardYou.getBoardSpot(y,x-1)).getBattleShip()).hit();
-                        GameboardYou.getBoardSpot(y,x-1).setIsHit(true);
-                        foundShip = true;
-                        counter = 0;
-                    } 
-                }
-                else
-                    counter++;
-            }
-            if (counter == 2) {
-                if(x<9) {
-                    if ((GameboardYou.getBoardSpot(y,x+1)).getContainsShip() == true && 
-                    (GameboardYou.getBoardSpot(y,x+1)).getIsHit() == false)
-                    {
-                        ((GameboardYou.getBoardSpot(y,x+1)).getBattleShip()).hit();
-                        GameboardYou.getBoardSpot(y,x+1).setIsHit(true);
-                        foundShip = true;
-                        counter = 0;
-                    } 
-                }
-                else
-                    counter++;
-            }
-            if (counter == 3) {
-                if(y>0) {
-                    if ((GameboardYou.getBoardSpot(y-1,x)).getContainsShip() == true && 
-                    (GameboardYou.getBoardSpot(y-1,x)).getIsHit() == false)
-                    {
-                        ((GameboardYou.getBoardSpot(y-1,x)).getBattleShip()).hit();
-                        GameboardYou.getBoardSpot(y-1,x).setIsHit(true);
-                        foundShip = true;
-                        counter = 0;
-                    } 
-                }
-                else
-                    counter++;
-            }
-            if (counter == 4) {
-                if(y<9) {
-                    if ((GameboardYou.getBoardSpot(y+1,x)).getContainsShip() == true && 
-                    (GameboardYou.getBoardSpot(y+1,x)).getIsHit() == false)
-                    {
-                        ((GameboardYou.getBoardSpot(y+1,x)).getBattleShip()).hit();
-                        GameboardYou.getBoardSpot(y+1,x).setIsHit(true);
-                        foundShip = true;
-                        counter = 0;
-                    } 
-                }
-                else
-                    counter++;
-            }
-            else {
-                counter = 0;
-                foundShip = false;
-            }
+        if (counter == 1) {
+        if(x>0) {
+        if ((GameboardYou.getBoardSpot(y,x-1)).getContainsShip() == true && 
+        (GameboardYou.getBoardSpot(y,x-1)).getIsHit() == false)
+        {
+        ((GameboardYou.getBoardSpot(y,x-1)).getBattleShip()).hit();
+        GameboardYou.getBoardSpot(y,x-1).setIsHit(true);
+        foundShip = true;
+        counter = 0;
+        } 
         }
+        else
+        counter++;
+        }
+        if (counter == 2) {
+        if(x<9) {
+        if ((GameboardYou.getBoardSpot(y,x+1)).getContainsShip() == true && 
+        (GameboardYou.getBoardSpot(y,x+1)).getIsHit() == false)
+        {
+        ((GameboardYou.getBoardSpot(y,x+1)).getBattleShip()).hit();
+        GameboardYou.getBoardSpot(y,x+1).setIsHit(true);
+        foundShip = true;
+        counter = 0;
+        } 
+        }
+        else
+        counter++;
+        }
+        if (counter == 3) {
+        if(y>0) {
+        if ((GameboardYou.getBoardSpot(y-1,x)).getContainsShip() == true && 
+        (GameboardYou.getBoardSpot(y-1,x)).getIsHit() == false)
+        {
+        ((GameboardYou.getBoardSpot(y-1,x)).getBattleShip()).hit();
+        GameboardYou.getBoardSpot(y-1,x).setIsHit(true);
+        foundShip = true;
+        counter = 0;
+        } 
+        }
+        else
+        counter++;
+        }
+        if (counter == 4) {
+        if(y<9) {
+        if ((GameboardYou.getBoardSpot(y+1,x)).getContainsShip() == true && 
+        (GameboardYou.getBoardSpot(y+1,x)).getIsHit() == false)
+        {
+        ((GameboardYou.getBoardSpot(y+1,x)).getBattleShip()).hit();
+        GameboardYou.getBoardSpot(y+1,x).setIsHit(true);
+        foundShip = true;
+        counter = 0;
+        } 
+        }
+        else
+        counter++;
+        }
+        else {
+        counter = 0;
+        foundShip = false;
+        }
+        }*/
     }
 
 }
